42cad6676f8e2aa81b655501acf140af4aecfb4f
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-10-31 18:42:00 +0330

    order line services added

--- App.Domain.Core/Products/Contracts/Services/IOrderlineServices.cs
+++ App.Domain.Core/Products/Contracts/Services/IOrderlineServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.Domain.Core.Products.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace App.Domain.Core.Products.Contracts.Services
 {
     public interface IOrderlineServices
     {
+        Task Create(OrderLineInputDto orderLineInputDto, CancellationToken cancellationToken);
+        Task Update(OrderLineInputDto orderLineInputDto, CancellationToken cancellationToken);
+        Task<List<OrderLineOutputDto>> GetAll(CancellationToken cancellationToken);
+        Task<OrderLineOutputDto> GetById(int Id, CancellationToken cancellationToken);
     }
 }
--- App.Domain.Services/OrderlineServices.cs
+++ App.Domain.Services/OrderlineServices.cs
@@ -1,4 +1,6 @@
-﻿using App.Domain.Core.Products.Contracts.Services;
+﻿using App.Domain.Core.Products.Contracts.Repositories;
+using App.Domain.Core.Products.Contracts.Services;
+using App.Domain.Core.Products.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,31 @@ namespace App.Domain.Services
 {
     public class OrderlineServices : IOrderlineServices
     {
+        private readonly IOrderlineRepository _repository;
+
+        public OrderlineServices(IOrderlineRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Create(OrderLineInputDto orderLineInputDto, CancellationToken cancellationToken)
+        {
+            await _repository.Create(orderLineInputDto, cancellationToken);
+        }
+
+        public async Task Update(OrderLineInputDto orderLineInputDto, CancellationToken cancellationToken)
+        {
+            await _repository.Update(orderLineInputDto, cancellationToken);
+        }
+
+        public async Task<List<OrderLineOutputDto>> GetAll(CancellationToken cancellationToken)
+        {
+            return await _repository.GetAll(cancellationToken);
+        }
+
+        public async Task<OrderLineOutputDto> GetById(int Id, CancellationToken cancellationToken)
+        {
+            return await _repository.GetById(Id, cancellationToken);
+        }
     }
 }
